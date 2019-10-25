57d533eb6bb15050ca6bcb596850082dac4a773e
Julio <julio.cusco@agence.cl>
2019-10-25 15:10:05 -0400

    Move global const to global.config file

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -2,6 +2,7 @@ import {Md5} from 'ts-md5/dist/md5';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {API_HOST, PRIVATE_KEY, PUBLIC_KEY} from '../../environments/global.conf';
 
 @Injectable()
 export class MarvelService {
@@ -10,9 +11,9 @@ export class MarvelService {
   private _privateKey;
 
   constructor(private _httpClient: HttpClient) {
-    this._privateKey = 'ce2dbaaa1cf946d6959dfc1f139306c8960fc880';
-    this._publicKey = '57c9261ef744947bb61e05a4956d5e2e';
-    this._apiHostUrl = 'https://gateway.marvel.com:443/v1/public/characters';
+    this._privateKey = PRIVATE_KEY;
+    this._publicKey = PUBLIC_KEY;
+    this._apiHostUrl = API_HOST;
   }
 
   private getHash(timeStamp: string): string {
---
+++ src/environments/global.conf.ts
@@ -0,0 +1,3 @@
+export const API_HOST = 'https://gateway.marvel.com:443/v1/public/characters';
+export const PUBLIC_KEY = '57c9261ef744947bb61e05a4956d5e2e';
+export const PRIVATE_KEY = 'ce2dbaaa1cf946d6959dfc1f139306c8960fc880';
