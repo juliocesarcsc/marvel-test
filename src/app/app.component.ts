74fba2f02e711f20a27ac9d04ae432a037524bf2
Julio <julio.cusco@agence.cl>
2019-10-25 14:59:54 -0400

    Update app component an template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Character} from './models/character.model';
+import {MarvelService} from './services/marvel.service';
+import {CustomResponse} from './models/response.model';
+import {HttpErrorResponse} from '@angular/common/http';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/app.component.css'],
+  providers: [
+    MarvelService
+  ]
 })
+
 export class AppComponent {
-  title = 'angular-unit-testing';
+  title: string;
+  character: Character;
+  characterId: number;
+
+  constructor(private _marvelService: MarvelService) {
+    this.title = 'Find Hero by ID';
+    this.characterId = null;
+    this.character = null;
+  }
+
+  handleError(response: HttpErrorResponse) {
+    if (response.status === 0) {
+      console.log('Service offline');
+    } else if (response.error && response.status && [401, 404, 402, 403, 405, 409].indexOf(response.status) !== -1) {
+      const errMsg = response.error.status ? response.error.status : response.error.message;
+      console.log(errMsg);
+    } else {
+      console.log('An unexpected error occurred.');
+    }
+  }
+
+  private findHero(characterId) {
+    if (!characterId) {
+      console.log('ID can\'t be null.');
+      return;
+    }
+    this._marvelService.getCharacter(characterId)
+      .pipe(finalize(() => console.log('finalize')))
+      .subscribe((response: CustomResponse<Character>) => {
+        console.log(response);
+        if (response.data && response.data.results && response.data.results.length) {
+          this.character = response.data.results[0];
+        }
+      }, error => this.handleError(error));
+  }
 }
