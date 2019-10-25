a00bf5095cf8ad2a65899bbd58503071a2459465
Julio <julio.cusco@agence.cl>
2019-10-25 15:08:09 -0400

    Add showMsg function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,31 +18,45 @@ export class AppComponent {
   title: string;
   character: Character;
   characterId: number;
+  showAlert: boolean;
+  alertText: string;
+  alertClass: string;
+  loading: boolean;
 
   constructor(private _marvelService: MarvelService) {
     this.title = 'Find Hero by ID';
     this.characterId = null;
     this.character = null;
+    this.showAlert = false;
+    this.alertText = null;
+    this.loading = false;
+  }
+
+  showMsg(msg: string, error: boolean = true) {
+    this.alertText = msg;
+    this.showAlert = true;
+    this.alertClass = error ? 'danger' : 'info';
   }
 
   handleError(response: HttpErrorResponse) {
     if (response.status === 0) {
-      console.log('Service offline');
+      this.showMsg('Service offline');
     } else if (response.error && response.status && [401, 404, 402, 403, 405, 409].indexOf(response.status) !== -1) {
       const errMsg = response.error.status ? response.error.status : response.error.message;
-      console.log(errMsg);
+      this.showMsg(errMsg);
     } else {
-      console.log('An unexpected error occurred.');
+      this.showMsg('An unexpected error occurred.');
     }
   }
 
   private findHero(characterId) {
     if (!characterId) {
-      console.log('ID can\'t be null.');
+      this.showMsg('ID can\'t be null.');
       return;
     }
+    this.loading = true;
     this._marvelService.getCharacter(characterId)
-      .pipe(finalize(() => console.log('finalize')))
+      .pipe(finalize(() => this.loading = false))
       .subscribe((response: CustomResponse<Character>) => {
         console.log(response);
         if (response.data && response.data.results && response.data.results.length) {
